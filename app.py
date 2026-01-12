09e3d956ef5dddec5f6a78857df109b6a1de1554
suraj <smileymaaa20@gmail.com>
2026-01-12 19:15:39 +0530

    main

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 # =============================
 # CONFIG  or"http://127.0.0.1:8000"
 # =============================
-API_BASE = "http://127.0.0.1:8000"
+API_BASE ="https://movie-recommendation-system-fastapi-nlp.onrender.com" or "http://127.0.0.1:8000"
 TMDB_IMG = "https://image.tmdb.org/t/p/w500"
 
 st.set_page_config(
